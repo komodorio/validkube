ed7a86261bcfd73d336615c711e193da38a4f235
sefi-infralight <71797160+sefi-infralight@users.noreply.github.com>
2022-03-06 14:42:20 +0200

    Create initial validiac fork (#1)
    This commit introduces an initial fork of validkube that provides
    similar capabilities for Terraform HCLs and related files.
    
    Four capabilities are provided:
    - lint: lints HCLs using tflint
    - secure: checks for security issues using tfsec
    - cost: calculates costs using infracost
    - map: generates graph using inframap
    
    The four dependencies are automatically downloaded using `make deps`.
    Their versions are defined in the Makefile.
    
    When the validiac binary is built, the location of the dependencies is
    injected using Go build-time variables.
    
    The binary can function both as an AWS Lambda function and as a simple
    command line interface.
    
    The UI has not been modified yet, and more work is required on the
    backend:
    
    - Infracost requires an API key for usage.
    - When building a lambda, injected dependency paths should probably be
      different than when build for local execution.
    - The tools mostly make sense when they can work on complete directories
      of Terraform files rather than individual files. This is not addressed
      yet.
    
    Co-authored-by: Ido Perlmuter <ido.perlmuter@gofirefly.io>

---
+++ backend/api/tfsec.go
@@ -0,0 +1,19 @@
+package api
+
+import (
+	"os"
+	"os/exec"
+)
+
+var TFSecExec = ""
+
+func TFSec(in []byte) ([]byte, error) {
+	path, err := asTempDir(".tf", in)
+	if err != nil {
+		return nil, err
+	}
+
+	defer os.RemoveAll(path) // nolint: errcheck
+
+	return exec.Command(TFSecExec, path).CombinedOutput()
+}
