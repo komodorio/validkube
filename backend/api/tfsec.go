ea220b7b48c6509e0a32083a67bee2c7a91e33e4
sefi-infralight <sefi@infralight.co>
2022-03-15 14:44:49 +0200

    Reducing redundant summary from TfSec output

--- backend/api/tfsec.go
+++ backend/api/tfsec.go
@@ -5,7 +5,7 @@ import (
 	"os/exec"
 )
 
-var TFSecExec = ""
+var TFSecExec = "./bin/tfsec-1.5.0"
 
 func TFSec(in []byte) ([]byte, error) {
 	path, err := asTempDir(".tf", in)
@@ -14,6 +14,5 @@ func TFSec(in []byte) ([]byte, error) {
 	}
 
 	defer os.RemoveAll(path) // nolint: errcheck
-
-	return exec.Command(TFSecExec, path, "--no-color").CombinedOutput()
+	return exec.Command(TFSecExec, path, "--concise-output", "--no-color").CombinedOutput()
 }
