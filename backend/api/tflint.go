2b977552a17539e885ff6c26103c55055bc91f49
sefi-infralight <71797160+sefi-infralight@users.noreply.github.com>
2022-03-29 11:10:06 +0300

    Updating TfLint execution arguments (#37)
    1. Adding HCL parser to the go.mod
    2.  Parsing HCL and running the command depends on the providers.
    aws enabled -> resource "aws_<RESOURCE>" "name"
    azure enabled -> resource "azure_<RESOURCE>" "name"
    gcp enabled -> resource "google_<RESOURCE>" "name"

--- backend/api/tflint.go
+++ backend/api/tflint.go
@@ -2,24 +2,84 @@ package api
 
 import (
     "fmt"
+    "github.com/hashicorp/hcl2/hcl/hclsyntax"
+    "github.com/hashicorp/hcl2/hclparse"
+    "github.com/thoas/go-funk"
+    "log"
     "os"
     "os/exec"
+    "strings"
 )
 
 var TFLintExec = getEnv("TFLINT_EXEC", fmt.Sprintf("%s/tflint", BIN_PATH))
 var TFLintConfig = getEnv("TFLINT_config", fmt.Sprintf("%s/.tflint.hcl", BIN_PATH))
 
-func tflintInit() ([]byte, error){
+func TFLint(in []byte) ([]byte, error) {
+    path, err := asTempDir(".tf", in)
+    if err != nil {
+        return nil, err
+    }
+
+    defer os.RemoveAll(path)
+
+	var hasAWS, hasAzure, hasGoogle = extractActiveProviders(in)
+
+    var args = []string{}
+    //Adding config args if one of the providers is enabled
+    if hasAWS||hasAzure||hasGoogle{
+        args = append(args, fmt.Sprintf("--config=%s", TFLintConfig))
+    }
+
+    if hasAWS{
+        args = append(args, "--enable-plugin=aws")
+    }
+
+    if hasAzure{
+        args = append(args, "--enable-plugin=azurerm")
+    }
+
+    if hasGoogle{
+        args = append(args, "--enable-plugin=google")
+    }
+
+    args = append(args, path)
+    args = append(args, "--no-color")
+    args = append(args, "--force")
+	return exec.Command(TFLintExec, args...).CombinedOutput()
+}
+
+func tflintInit() ([]byte, error) {
     var cmd = exec.Command(TFLintExec, "--init", "-c", TFLintConfig)
     return cmd.CombinedOutput()
 }
 
-func TFLint(in []byte) ([]byte, error) {
-	path, err := asTempFile("", ".tf", in)
-	if err != nil {
-		return nil, err
-	}
+func extractActiveProviders(hcl []byte) (bool, bool, bool) {
+    parser := hclparse.NewParser()
+    f, parseDiags := parser.ParseHCL(hcl, "")
+    if parseDiags.HasErrors() {
+        log.Fatal(parseDiags.Error())
+    }
 
-	defer os.Remove(path) // nolint: errcheck
-    return exec.Command(TFLintExec, path, "--no-color", "--force").CombinedOutput()
+    var hasAWS = false
+    var hasAzure = false
+    var hasGoogle = false
+
+    var blocks = funk.GetOrElse(funk.Get(f.Body, "Blocks"), hclsyntax.Blocks{}).(hclsyntax.Blocks)
+    funk.ForEach(blocks, func(block *hclsyntax.Block) {
+        if block.Type == "resource" && len(block.Labels) > 0 {
+            if strings.HasPrefix(block.Labels[0], "aws") {
+                hasAWS = true
+            }
+
+            if strings.HasPrefix(block.Labels[0], "azure") {
+                hasAzure = true
+            }
+
+            if strings.HasPrefix(block.Labels[0], "google") {
+                hasGoogle = true
+            }
+        }
+    })
+
+    return hasAWS, hasAzure, hasGoogle
 }
--- go.mod
+++ go.mod
@@ -6,12 +6,19 @@ require (
 	github.com/alecthomas/kong v0.5.0
 	github.com/aquasecurity/lmdrouter v0.4.4
 	github.com/aws/aws-lambda-go v1.28.0
+	github.com/hashicorp/hcl2 v0.0.0-20191002203319-fb75b3253c80
 	github.com/jgroeneveld/trial v2.0.0+incompatible
 	github.com/thoas/go-funk v0.9.2
 
 )
 
 require (
+	github.com/agext/levenshtein v1.2.1 // indirect
+	github.com/apparentlymart/go-textseg v1.0.0 // indirect
+	github.com/google/go-cmp v0.2.0 // indirect
 	github.com/jgroeneveld/schema v1.0.0 // indirect
+	github.com/mitchellh/go-wordwrap v0.0.0-20150314170334-ad45545899c7 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/zclconf/go-cty v1.0.0 // indirect
+	golang.org/x/text v0.3.2 // indirect
 )
