b75c7f7d1bc9743263d8b6eb635c5e7b9d809067
Nir Shtein <89006520+nirsht@users.noreply.github.com>
2023-03-06 13:32:03 +0200

    Formatting and pretty rules

--- frontend/src/components/MainView/YamlBox/CodemirrorKomodorTheme.tsx
+++ frontend/src/components/MainView/YamlBox/CodemirrorKomodorTheme.tsx
@@ -1,135 +1,147 @@
-import {EditorView} from "@codemirror/view";
-import {HighlightStyle, tags as t} from "@codemirror/highlight"
-import {Extension} from "@codemirror/state"
+import { EditorView } from "@codemirror/view";
+import { HighlightStyle, tags as t } from "@codemirror/highlight";
+import { Extension } from "@codemirror/state";
 
 const chalky = "#e5c07b",
-    coral = "#e06c75",
-    cyan = "#56b6c2",
-    invalid = "#ffffff",
-    ivory = "#abb2bf",
-    stone = "#7d8799",
-    malibu = "#61afef",
-    sage = "#98c379",
-    whiskey = "#007AFF !important",
-    violet = "#c678dd",
-    darkBackground = "#21252b",
-    highlightBackground = "#2c313a",
-    background = "#282c34",
-    tooltipBackground = "#353a42",
-    selection = "#3E4451",
-    cursor = "#528bff"
+  coral = "#e06c75",
+  cyan = "#56b6c2",
+  invalid = "#ffffff",
+  ivory = "#abb2bf",
+  stone = "#7d8799",
+  malibu = "#61afef",
+  sage = "#98c379",
+  whiskey = "#007AFF !important",
+  violet = "#c678dd",
+  darkBackground = "#21252b",
+  highlightBackground = "#2c313a",
+  background = "#282c34",
+  tooltipBackground = "#353a42",
+  selection = "#3E4451",
+  cursor = "#528bff";
 
 /// The editor theme styles for One Dark.
-export const KomodoTheme = EditorView.theme({
-  "&": {
-    color: ivory,
-    backgroundColor: background,
-    background: "transparent !important",
-    padding: "15px"
-  },
+export const KomodoTheme = EditorView.theme(
+  {
+    "&": {
+      color: ivory,
+      backgroundColor: background,
+      background: "transparent !important",
+      padding: "15px",
+    },
 
-  ".cm-content": {
-    caretColor: cursor,
-    background: "transparent !important",
-    fontFamily: "Roboto Mono",
-    fontSize: "14px",
-  },
+    ".cm-content": {
+      caretColor: cursor,
+      background: "transparent !important",
+      fontFamily: "Roboto Mono",
+      fontSize: "14px",
+    },
 
-  ".cm-cursor, .cm-dropCursor": {borderLeftColor: cursor},
-  "&.cm-focused .cm-selectionBackground, .cm-selectionBackground, .cm-content ::selection": {backgroundColor: selection},
+    ".cm-cursor, .cm-dropCursor": { borderLeftColor: cursor },
+    "&.cm-focused .cm-selectionBackground, .cm-selectionBackground, .cm-content ::selection":
+      { backgroundColor: selection },
 
-  ".cm-panels": {backgroundColor: darkBackground, color: ivory},
-  ".cm-panels.cm-panels-top": {borderBottom: "2px solid black"},
-  ".cm-panels.cm-panels-bottom": {borderTop: "2px solid black"},
+    ".cm-panels": { backgroundColor: darkBackground, color: ivory },
+    ".cm-panels.cm-panels-top": { borderBottom: "2px solid black" },
+    ".cm-panels.cm-panels-bottom": { borderTop: "2px solid black" },
 
-  ".cm-searchMatch": {
-    backgroundColor: "#72a1ff59",
-    outline: "1px solid #457dff"
-  },
-  ".cm-searchMatch.cm-searchMatch-selected": {
-    backgroundColor: "#6199ff2f"
-  },
+    ".cm-searchMatch": {
+      backgroundColor: "#72a1ff59",
+      outline: "1px solid #457dff",
+    },
+    ".cm-searchMatch.cm-searchMatch-selected": {
+      backgroundColor: "#6199ff2f",
+    },
 
-  ".cm-activeLine": {backgroundColor: "transparent !important"},
-  ".cm-selectionMatch": {backgroundColor: "#aafe661a"},
+    ".cm-activeLine": { backgroundColor: "transparent !important" },
+    ".cm-selectionMatch": { backgroundColor: "#aafe661a" },
 
-  "&.cm-focused .cm-matchingBracket, &.cm-focused .cm-nonmatchingBracket": {
-    backgroundColor: "#bad0f847",
-    outline: "1px solid #515a6b"
-  },
+    "&.cm-focused .cm-matchingBracket, &.cm-focused .cm-nonmatchingBracket": {
+      backgroundColor: "#bad0f847",
+      outline: "1px solid #515a6b",
+    },
 
-  ".cm-gutters": {
-    background: "transparent !important",
-    color: stone,
-    border: "none",
-  },
+    ".cm-gutters": {
+      background: "transparent !important",
+      color: stone,
+      border: "none",
+    },
 
-  ".cm-activeLineGutter": {
-    backgroundColor: "transparent !important"
-  },
+    ".cm-activeLineGutter": {
+      backgroundColor: "transparent !important",
+    },
 
-  ".cm-foldPlaceholder": {
-    backgroundColor: "transparent",
-    border: "none",
-    color: "#ddd"
-  },
+    ".cm-foldPlaceholder": {
+      backgroundColor: "transparent",
+      border: "none",
+      color: "#ddd",
+    },
 
-  ".cm-tooltip": {
-    border: "none",
-    backgroundColor: tooltipBackground
+    ".cm-tooltip": {
+      border: "none",
+      backgroundColor: tooltipBackground,
+    },
+    ".cm-tooltip .cm-tooltip-arrow:before": {
+      borderTopColor: "transparent",
+      borderBottomColor: "transparent",
+    },
+    ".cm-tooltip .cm-tooltip-arrow:after": {
+      borderTopColor: tooltipBackground,
+      borderBottomColor: tooltipBackground,
+    },
+    ".cm-tooltip-autocomplete": {
+      "& > ul > li[aria-selected]": {
+        backgroundColor: highlightBackground,
+        color: ivory,
+      },
+    },
   },
-  ".cm-tooltip .cm-tooltip-arrow:before": {
-    borderTopColor: "transparent",
-    borderBottomColor: "transparent"
-  },
-  ".cm-tooltip .cm-tooltip-arrow:after": {
-    borderTopColor: tooltipBackground,
-    borderBottomColor: tooltipBackground
-  },
-  ".cm-tooltip-autocomplete": {
-    "& > ul > li[aria-selected]": {
-      backgroundColor: highlightBackground,
-      color: ivory
-    }
-  }
-}, {dark: true})
+  { dark: true }
+);
 
 /// The highlighting style for code in the One Dark theme.
 export const KomodoHighlightStyle = HighlightStyle.define([
-  {tag: t.keyword,
-    color: violet},
-  {tag: [t.name, t.deleted, t.character, t.propertyName, t.macroName],
-    color: coral},
-  {tag: [t.function(t.variableName), t.labelName],
-    color: malibu},
-  {tag: [t.color, t.constant(t.name), t.standard(t.name)],
-    color: whiskey},
-  {tag: [t.definition(t.name), t.separator],
-    color: ivory},
-  {tag: [t.typeName, t.className, t.number, t.changed, t.annotation, t.modifier, t.self, t.namespace],
-    color: chalky},
-  {tag: [t.operator, t.operatorKeyword, t.url, t.escape, t.regexp, t.link, t.special(t.string)],
-    color: cyan},
-  {tag: [t.meta, t.comment],
-    color: stone},
-  {tag: t.strong,
-    fontWeight: "bold"},
-  {tag: t.emphasis,
-    fontStyle: "italic"},
-  {tag: t.strikethrough,
-    textDecoration: "line-through"},
-  {tag: t.link,
-    color: stone,
-    textDecoration: "underline"},
-  {tag: t.heading,
-    fontWeight: "bold",
-    color: coral},
-  {tag: [t.atom, t.bool, t.special(t.variableName)],
-    color: whiskey },
-  {tag: [t.processingInstruction, t.string, t.inserted],
-    color: sage},
-  {tag: t.invalid,
-    color: invalid},
-])
+  { tag: t.keyword, color: violet },
+  {
+    tag: [t.name, t.deleted, t.character, t.propertyName, t.macroName],
+    color: coral,
+  },
+  { tag: [t.function(t.variableName), t.labelName], color: malibu },
+  { tag: [t.color, t.constant(t.name), t.standard(t.name)], color: whiskey },
+  { tag: [t.definition(t.name), t.separator], color: ivory },
+  {
+    tag: [
+      t.typeName,
+      t.className,
+      t.number,
+      t.changed,
+      t.annotation,
+      t.modifier,
+      t.self,
+      t.namespace,
+    ],
+    color: chalky,
+  },
+  {
+    tag: [
+      t.operator,
+      t.operatorKeyword,
+      t.url,
+      t.escape,
+      t.regexp,
+      t.link,
+      t.special(t.string),
+    ],
+    color: cyan,
+  },
+  { tag: [t.meta, t.comment], color: stone },
+  { tag: t.strong, fontWeight: "bold" },
+  { tag: t.emphasis, fontStyle: "italic" },
+  { tag: t.strikethrough, textDecoration: "line-through" },
+  { tag: t.link, color: stone, textDecoration: "underline" },
+  { tag: t.heading, fontWeight: "bold", color: coral },
+  { tag: [t.atom, t.bool, t.special(t.variableName)], color: whiskey },
+  { tag: [t.processingInstruction, t.string, t.inserted], color: sage },
+  { tag: t.invalid, color: invalid },
+]);
 
-export const komodo: Extension = [KomodoTheme, KomodoHighlightStyle]
+export const komodo: Extension = [KomodoTheme, KomodoHighlightStyle];
