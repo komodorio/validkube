c70524a7c36b162bfce78366b86ab0633cbebadd
ElisarEisenbach <elisar@komodor.io>
2023-05-08 16:43:27 +0300

    use sendAnalytics function

--- frontend/src/components/MainView/index.tsx
+++ frontend/src/components/MainView/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, {useCallback, useState} from "react";
 import styled from "styled-components";
 import { callAPI } from "../../services/api-service";
 import MainViewHeader from "./MainViewHeader";
